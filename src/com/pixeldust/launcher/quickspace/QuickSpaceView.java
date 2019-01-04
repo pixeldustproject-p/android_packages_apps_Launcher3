CypherOS/packages_apps_Luna

Quickspace: Create action receivers for date/weather view
Change-Id: I2e2d8e549e5868beb92772b038f5a6719cf598c3

src/co/aoscp/lovegood/quickspace/QuickSpaceView.java
@@ -36,11 +36,15 @@ import android.widget.ImageView;
 import android.widget.FrameLayout;
 import android.widget.TextView;
 
+import co.aoscp.lovegood.Bits;
+import co.aoscp.lovegood.LunaLauncher.LunaLauncherCallbacks;
+import co.aoscp.lovegood.quickspace.receivers.QuickSpaceActionReceiver;
 import co.aoscp.lovegood.views.DateTextView;
 
 import com.android.internal.ambient.weather.WeatherClient;
 import com.android.launcher3.BubbleTextView;
 import com.android.launcher3.ItemInfo;
+import com.android.launcher3.Launcher;
 import com.android.launcher3.R;
 
 public class QuickSpaceView extends FrameLayout implements ValueAnimator.AnimatorUpdateListener, WeatherClient.WeatherObserver, Runnable {
@@ -48,6 +52,7 @@ public class QuickSpaceView extends FrameLayout implements ValueAnimator.Animato
     private static final String SETTING_WEATHER_LOCKSCREEN_UNIT = "weather_lockscreen_unit";
 
     protected ContentResolver mContentResolver;
+	protected Context mContext;
 
     private BubbleTextView mBubbleTextView;
     private DateTextView mClockView;
@@ -62,9 +67,12 @@ public class QuickSpaceView extends FrameLayout implements ValueAnimator.Animato
     private WeatherClient.WeatherInfo mWeatherInfo;
     private WeatherSettingsObserver mWeatherSettingsObserver;
     private boolean mUseImperialUnit;
+	
+	private QuickSpaceActionReceiver mActionReceiver;
 
     public QuickSpaceView(Context context, AttributeSet set) {
         super(context, set);
+		mContext = context;
         mHandler = new Handler();
         if (WeatherClient.isAvailable(context)) {
             mWeatherSettingsObserver = new WeatherSettingsObserver(
@@ -74,6 +82,8 @@ public class QuickSpaceView extends FrameLayout implements ValueAnimator.Animato
             mWeatherClient = new WeatherClient(getContext());
             mWeatherClient.addObserver(this);
         }
+
+		mActionReceiver = new QuickSpaceActionReceiver(context);
     }
 
     private void initListeners() {
@@ -82,6 +92,9 @@ public class QuickSpaceView extends FrameLayout implements ValueAnimator.Animato
 
     private void loadSingleLine() {
         setBackgroundResource(0);
+		boolean hasGoogleApp = Bits.hasPackageInstalled(Launcher.getLauncher(mContext), LunaLauncherCallbacks.SEARCH_PACKAGE);
+		boolean hasGoogleCalendar = Bits.hasPackageInstalled(Launcher.getLauncher(mContext), "com.google.android.calendar");
+		mClockView.setOnClickListener(hasGoogleCalendar ? mActionReceiver.getCalendarAction() : null);
         if (!WeatherClient.isAvailable(getContext())) {
             mWeatherContent.setVisibility(View.GONE);
             mSeparator.setVisibility(View.GONE);
@@ -111,6 +124,7 @@ public class QuickSpaceView extends FrameLayout implements ValueAnimator.Animato
         mSeparator.setVisibility(View.VISIBLE);
         mWeatherContent.setVisibility(View.VISIBLE);
         mWeatherTemp.setText(temperatureText);
+		mWeatherTemp.setOnClickListener(hasGoogleApp ? mActionReceiver.getWeatherAction() : null);
         mWeatherIcon.setImageIcon(conditionIcon);
     }
 

src/co/aoscp/lovegood/quickspace/receivers/QuickSpaceActionReceiver.java
@@ -1,0 +1,94 @@
+/*
+ * Copyright (C) 2018 CypherOS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package co.aoscp.lovegood.quickspace.receivers;
+
+import android.content.ActivityNotFoundException;
+import android.content.ComponentName;
+import android.content.ContentUris;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Process;
+import android.os.UserHandle;
+import android.provider.CalendarContract;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+import co.aoscp.lovegood.LunaLauncher.LunaLauncherCallbacks;
+
+import com.android.launcher3.Launcher;
+import com.android.launcher3.R;
+import com.android.launcher3.compat.LauncherAppsCompat;
+
+public class QuickSpaceActionReceiver {
+
+	private static Context mContext;
+
+	public OnClickListener mCalendarClickListener;
+	public OnClickListener mWeatherClickListener;
+
+    public QuickSpaceActionReceiver(Context context) {
+		mContext = context;
+
+		mCalendarClickListener = new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+				openGoogleCalendar(view);
+            }
+        };
+		
+		mWeatherClickListener = new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+				openGoogleWeather(view);
+            }
+        };
+    }
+
+	private void openGoogleCalendar(View view) {
+		final Uri content_URI = CalendarContract.CONTENT_URI;
+        final Uri.Builder appendPath = content_URI.buildUpon().appendPath("time");
+        ContentUris.appendId(appendPath, System.currentTimeMillis());
+        final Intent addFlags = new Intent(Intent.ACTION_VIEW)
+                .setData(appendPath.build())
+                .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
+        try {
+            Launcher.getLauncher(mContext).startActivitySafely(view, addFlags, null);
+        } catch (ActivityNotFoundException ex) {
+            LauncherAppsCompat.getInstance(mContext).showAppDetailsForProfile(new ComponentName(LunaLauncherCallbacks.SEARCH_PACKAGE, ""), Process.myUserHandle());
+		}
+    }
+
+	private void openGoogleWeather(View view) {
+		Intent intent = new Intent("android.intent.action.VIEW");
+		intent.setData(Uri.parse("dynact://velour/weather/ProxyActivity"));
+		intent.setComponent(new ComponentName(LunaLauncherCallbacks.SEARCH_PACKAGE, "com.google.android.apps.gsa.velour.DynamicActivityTrampoline"));
+		try {
+            Launcher.getLauncher(mContext).startActivitySafely(view, intent, null);
+        } catch (ActivityNotFoundException ex) {
+            LauncherAppsCompat.getInstance(mContext).showAppDetailsForProfile(new ComponentName(LunaLauncherCallbacks.SEARCH_PACKAGE, 
+			        "com.google.android.apps.gsa.velour.DynamicActivityTrampoline"), Process.myUserHandle());
+		}
+    }
+
+	public OnClickListener getCalendarAction() {
+		return mCalendarClickListener;
+	}
+
+	public OnClickListener getWeatherAction() {
+		return mWeatherClickListener;
+	}
+}

pixeldustproject-p/android_packages_apps_Launcher3

Signed-off-by: spezi77 <spezi7713@gmx.net>

src/com/pixeldust/launcher/quickspace/QuickSpaceView.java
@@ -38,11 +38,15 @@ import android.widget.FrameLayout;
 import android.widget.TextView;
 
 import com.pixeldust.launcher.views.DateTextView;
+import com.pixeldust.launcher.PixelDustLauncher.PixelDustLauncherCallbacks;
+import com.pixeldust.launcher.quickspace.receivers.QuickSpaceActionReceiver;
 
 import com.android.internal.util.weather.WeatherClient;
 import com.android.launcher3.BubbleTextView;
 import com.android.launcher3.ItemInfo;
+import com.android.launcher3.Launcher;
 import com.android.launcher3.R;
+import com.android.launcher3.Utilities;
 
 public class QuickSpaceView extends FrameLayout implements ValueAnimator.AnimatorUpdateListener, WeatherClient.WeatherObserver, Runnable {
 
@@ -50,6 +54,7 @@ public class QuickSpaceView extends FrameLayout implements ValueAnimator.Animato
     private static final String FONT_FAMILY_MEDIUM = "sans-serif-medium";
 
     protected ContentResolver mContentResolver;
+    protected Context mContext;
 
     private BubbleTextView mBubbleTextView;
     private DateTextView mClockView;
@@ -65,8 +70,11 @@ public class QuickSpaceView extends FrameLayout implements ValueAnimator.Animato
     private WeatherSettingsObserver mWeatherSettingsObserver;
     private boolean mUseImperialUnit;
 
+    private QuickSpaceActionReceiver mActionReceiver;
+
     public QuickSpaceView(Context context, AttributeSet set) {
         super(context, set);
+        mContext = context;
         mHandler = new Handler();
         if (WeatherClient.isAvailable(context)) {
             mWeatherSettingsObserver = new WeatherSettingsObserver(
@@ -76,6 +84,8 @@ public class QuickSpaceView extends FrameLayout implements ValueAnimator.Animato
             mWeatherClient = new WeatherClient(getContext());
             mWeatherClient.addObserver(this);
         }
+
+        mActionReceiver = new QuickSpaceActionReceiver(context);
     }
 
     private void initListeners() {
@@ -84,6 +94,9 @@ public class QuickSpaceView extends FrameLayout implements ValueAnimator.Animato
 
     private void loadSingleLine() {
         setBackgroundResource(0);
+        boolean hasGoogleApp = Bits.hasPackageInstalled(Launcher.getLauncher(mContext), PixelDustLauncherCallbacks.SEARCH_PACKAGE);
+        boolean hasGoogleCalendar = Bits.hasPackageInstalled(Launcher.getLauncher(mContext), "com.google.android.calendar");
+        mClockView.setOnClickListener(hasGoogleCalendar ? mActionReceiver.getCalendarAction() : null);
         if (!WeatherClient.isAvailable(getContext())) {
             mWeatherContent.setVisibility(View.GONE);
             mSeparator.setVisibility(View.GONE);
@@ -113,6 +126,7 @@ public class QuickSpaceView extends FrameLayout implements ValueAnimator.Animato
         mSeparator.setVisibility(View.VISIBLE);
         mWeatherContent.setVisibility(View.VISIBLE);
         mWeatherTemp.setText(temperatureText);
+        mWeatherTemp.setOnClickListener(hasGoogleApp ? mActionReceiver.getWeatherAction() : null);
         mWeatherIcon.setImageIcon(conditionIcon);
     }
 

src/com/pixeldust/launcher/quickspace/receivers/QuickSpaceActionReceiver.java
@@ -1,0 +1,95 @@
+/*
+ * Copyright (C) 2018 CypherOS
+ * Copyright (C) 2018 The Pixel Dust Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.pixeldust.launcher.quickspace.receivers;
+
+import android.content.ActivityNotFoundException;
+import android.content.ComponentName;
+import android.content.ContentUris;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Process;
+import android.os.UserHandle;
+import android.provider.CalendarContract;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+import com.pixeldust.launcher.PixelDustLauncher.PixelDustLauncherCallbacks;
+
+import com.android.launcher3.Launcher;
+import com.android.launcher3.R;
+import com.android.launcher3.compat.LauncherAppsCompat;
+
+public class QuickSpaceActionReceiver {
+
+    private static Context mContext;
+
+    public OnClickListener mCalendarClickListener;
+    public OnClickListener mWeatherClickListener;
+
+    public QuickSpaceActionReceiver(Context context) {
+        mContext = context;
+
+        mCalendarClickListener = new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openGoogleCalendar(view);
+            }
+        };
+
+        mWeatherClickListener = new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openGoogleWeather(view);
+            }
+        };
+    }
+
+    private void openGoogleCalendar(View view) {
+        final Uri content_URI = CalendarContract.CONTENT_URI;
+        final Uri.Builder appendPath = content_URI.buildUpon().appendPath("time");
+        ContentUris.appendId(appendPath, System.currentTimeMillis());
+        final Intent addFlags = new Intent(Intent.ACTION_VIEW)
+                .setData(appendPath.build())
+                .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
+        try {
+            Launcher.getLauncher(mContext).startActivitySafely(view, addFlags, null);
+        } catch (ActivityNotFoundException ex) {
+            LauncherAppsCompat.getInstance(mContext).showAppDetailsForProfile(new ComponentName(PixelDustLauncherCallbacks.SEARCH_PACKAGE, ""), Process.myUserHandle());
+        }
+    }
+
+    private void openGoogleWeather(View view) {
+        Intent intent = new Intent("android.intent.action.VIEW");
+        intent.setData(Uri.parse("dynact://velour/weather/ProxyActivity"));
+        intent.setComponent(new ComponentName(PixelDustLauncherCallbacks.SEARCH_PACKAGE, "com.google.android.apps.gsa.velour.DynamicActivityTrampoline"));
+        try {
+            Launcher.getLauncher(mContext).startActivitySafely(view, intent, null);
+        } catch (ActivityNotFoundException ex) {
+            LauncherAppsCompat.getInstance(mContext).showAppDetailsForProfile(new ComponentName(PixelDustLauncherCallbacks.SEARCH_PACKAGE, 
+                    "com.google.android.apps.gsa.velour.DynamicActivityTrampoline"), Process.myUserHandle());
+        }
+    }
+
+    public OnClickListener getCalendarAction() {
+        return mCalendarClickListener;
+    }
+
+    public OnClickListener getWeatherAction() {
+        return mWeatherClickListener;
+    }
+}